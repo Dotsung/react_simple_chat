d1e7051f2d55b124d7ba7ccfa641bd25ac783317
Dotsung <andy395023@gmail.com>
2019-03-27 09:07:21 +0900

    init

---
+++ front/src/components/App.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+class App extends Component {
+
+  render() {
+      return (
+          <div>
+              a
+          </div>
+      );
+  }
+}
+
+export default App;
